0b918160e9ad4526d583860b0f23e82ba5689c5b
Mateus Macedo Dos Anjos <mateus.anjos@conexia.com.br>
2022-08-31 13:17:34 -0300

    chore: jest config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '@lib/(.*)': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  testResultsProcessor: 'jest-sonar-reporter',
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: 80
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}']
+}
