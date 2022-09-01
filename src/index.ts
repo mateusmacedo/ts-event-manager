15ff8149402ae5856a8d8fe9176b40978618515e
Mateus Macedo Dos Anjos <mateus.anjos@conexia.com.br>
2022-09-01 08:18:24 -0300

    chore: add module alias package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-if (!process.env.IS_TS_NODE) {
-  require('module-alias/register')
-}
+require('module-alias/register')
 
 export * from '@lib/main/factory/EventBroadcasterFactory'
 export * from '@lib/main/factory/EventManagerFactory'
