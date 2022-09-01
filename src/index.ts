285bcdf94630ebce773c693d12073184ec989e1e
Mateus Macedo Dos Anjos <mateus.anjos@conexia.com.br>
2022-09-01 09:55:54 -0300

    chore: retry config paths

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-require('module-alias/register')
-
+import 'module-alias/register'
 export * from '@lib/main/factory/EventBroadcasterFactory'
 export * from '@lib/main/factory/EventManagerFactory'
 export * from '@lib/infra/BroadcasterDecorator'
